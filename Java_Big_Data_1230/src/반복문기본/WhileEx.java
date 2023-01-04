784808911dc4f620513d98a1a326e171a6703ad8
fearofgod0001 <dlxo8888@gmail.com>
2023-01-04 14:55:39 +0900

    반복문 연습 : 나무 찍기

---
+++ Java_Big_Data_1230/src/반복문기본/WhileEx.java
@@ -0,0 +1,13 @@
+package 반복문기본;
+
+public class WhileEx {
+    public static void main(String[] args) {
+        int treeHit = 0;
+        while(treeHit < 10){
+            treeHit++;
+            System.out.println("나무 찍는 중.. " +treeHit + "번");
+            if(treeHit == 10) System.out.println("나무 넘어 갑니다");
+        }
+    }
+
+}
