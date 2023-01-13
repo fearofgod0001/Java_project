10e43e2f54cf4ca577dc190fe86591510a6f36a9
fearofgod0001 <dlxo8888@gmail.com>
2023-01-13 12:17:19 +0900

    상속

---
+++ Java_Big_Data_1230/src/상속기본/BasicInheritance.java
@@ -0,0 +1,10 @@
+package 상속기본;
+
+public class BasicInheritance {
+    public static void main(String[] args) {
+        HouseDog hd = new HouseDog();
+        hd.sleep();
+        hd.sleep(4);
+
+    }
+}
