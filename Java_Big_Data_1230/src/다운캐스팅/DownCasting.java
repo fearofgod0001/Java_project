4538a3332d1e62f50ab3fab366c572f14e4e0ff0
fearofgod0001 <dlxo8888@gmail.com>
2023-01-25 14:53:08 +0900

    형변환 다운캐스팅

---
+++ Java_Big_Data_1230/src/다운캐스팅/DownCasting.java
@@ -0,0 +1,36 @@
+package 다운캐스팅;
+
+import java.util.ArrayList;
+
+// 다운캐스팅? 상위 클래스형으로 변환되었던 하위 클래스를 다시 원래 자료형으로 변환하는 것을 다운캐스팅
+// instanceof : 형 변환 가능여부를 확인하는 용도(참조 변수가 참조하고 있는 객채의 실체 타입을
+public class DownCasting {
+    ArrayList<Animal> aniList = new ArrayList<>();
+    public static void main(String[] args) {
+        DownCasting downCasting = new DownCasting();
+
+    }
+    public void addAnimal(){
+        aniList.add(new Human());//ArrayList에 추가되면서 타입이 Animal형으로 변환
+        aniList.add(new Tiger());
+        aniList.add(new Eagle());
+        for(Animal e : aniList) e.move();
+    }
+    public void testCasting(){
+        for(int i = 0; i < aniList.size(); i++){
+            Animal ani = aniList.get(i);
+            if(ani instanceof Human){ // Human 타입으로 형 변환 가능 여부를 물어 봄
+                Human h =(Human) ani;
+                h.reaBook();
+            } else if(ani instanceof Tiger){
+                Tiger t = (Tiger) ani;
+                t.hunting();
+            }else if(ani instanceof Eagle){
+                Eagle e =(Eagle) ani;
+                e.flying();
+            }else{
+                System.out.println("지원 되지 않는 형 입니다");
+            }
+        }
+    }
+}
