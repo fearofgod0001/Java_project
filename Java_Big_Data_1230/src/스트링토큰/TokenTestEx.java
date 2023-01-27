8358e4b3ab563dc5f2898dc074962e01000486ab
fearofgod0001 <dlxo8888@gmail.com>
2023-01-27 14:02:10 +0900

    스플릿과 토큰

---
+++ Java_Big_Data_1230/src/스트링토큰/TokenTestEx.java
@@ -0,0 +1,30 @@
+package 스트링토큰;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// StringTokenizer : 문자열이 특정 구분자로 연결되어있는 경우, 구분자 기준으로 문자열을 나눔
+// countTokens() : 남아있는 토큰 수
+// hasMoreTokens() : 남아 있는 토큰이 있는지 여부 확인(있으면 true)
+// nextToken() : 토큰을 하나씩 꺼내 옴
+public class TokenTestEx {
+    public static void main(String[] args) {
+        int idx = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("이름을 / 기준으로 입력 : ");
+        String name = sc.nextLine(); // 문자열이 개행문자를 만날 때 까지 입력 받음
+//        StringTokenizer st = new StringTokenizer(name,"/");
+//        String[] nameStr = new String[st.countTokens()]; // 토큰의 개수 만큼의 문자열 배열 생성
+//        while(st.hasMoreTokens()){ // 남아있는 토큰이 있는 동안 반복문 수행
+//            nameStr[idx++] = st.nextToken(); //현재 토큰을 꺼내서 배열에 담음
+//            }
+//        System.out.println(Arrays.toString(nameStr));
+//
+
+        String[] nameStrr = name.split("/");
+        System.out.println(Arrays.toString(nameStrr));
+
+
+    }
+}
