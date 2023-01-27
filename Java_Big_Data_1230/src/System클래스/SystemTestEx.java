262dc7e7a5c03754ab6a769c55c1f3c0312606b6
fearofgod0001 <dlxo8888@gmail.com>
2023-01-27 12:09:06 +0900

    시스템 메소드 예제

---
+++ Java_Big_Data_1230/src/System클래스/SystemTestEx.java
@@ -0,0 +1,30 @@
+package System클래스;
+
+import static java.lang.Thread.sleep;
+
+//경과 시간 측정 하기
+public class SystemTestEx {
+    public static void main(String[] args) throws InterruptedException{
+        int[] data = new int[1000];
+        int cnt = 0;
+        int val = (int)(Math.random() * 1000) +1; // 1~1000 사이 임의 값
+        for(int i =0;i<data.length; i++){ // 1000개의 배열에 1부터 1000까지 값을 저장
+            data[i] = i +1;
+        }
+
+        long time1 = System.currentTimeMillis(); //측정을 위한 시작 시간 가져오기
+        for(int i = 0; i< data.length; i++){
+            cnt++;
+            sleep(1); //시간 측정에 대한 지연 요인을 만들기 위해 인위적으로
+            if(val == data[i]){
+                System.out.println("위치 : " + (i+1));
+                break;
+
+            }
+        }
+        long time2 = System.currentTimeMillis(); //측정을 위한 시작 시간 가져오기
+        System.out.println("총 검색 횟수 : " + cnt);
+        System.out.println("검색에 소요된 시간 : " + (time2 - time1) + "밀리초");
+
+    }
+}
