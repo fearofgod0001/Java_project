74653b07fb172f13dbe91a507784e6e9acc22bcd
fearofgod0001 <dlxo8888@gmail.com>
2023-02-03 10:06:10 +0900

    예제

--- Java_Big_Data_1230/src/커피메뉴만들기/MenuInfo.java
+++ Java_Big_Data_1230/src/커피메뉴만들기/MenuInfo.java
@@ -6,6 +6,22 @@ public class MenuInfo implements Comparable<MenuInfo>{
     String group;
     String desc;
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
     public MenuInfo(String name, int price, String group, String desc) {
         this.name = name;
         this.price = price;
