da46178db932f5931358397413b555c11e5ce1a5
fearofgod0001 <dlxo8888@gmail.com>
2023-06-05 10:04:06 +0900

    0605

--- Java_Big_Data_1230/src/다형성복습/Driver.java
+++ Java_Big_Data_1230/src/다형성복습/Driver.java
@@ -1,4 +1,13 @@
 package 다형성복습;
 
 public class Driver {
+    String name;
+    public Driver(String name){
+        this.name = name;
+
+    }
+    public void drive(Vehicle vehicle){
+        System.out.print(name + "의 ");
+        vehicle.run();
+    }
 }
--- Java_Big_Data_1230/src/다형성복습/PolyEx05.java
+++ Java_Big_Data_1230/src/다형성복습/PolyEx05.java
@@ -1,10 +1,25 @@
 package 다형성복습;
 
+import java.util.Scanner;
+
 // 다형성의 특성을 활용하여 여러가지 타입을 한가지 타입으로 처리하는 기술
 // 즉, 부모 클래스 타입의 참조 변수로 자식 클래스의 인스턴스를 참조할 수 있음.
 public class PolyEx05 {
     public static void main(String[] args) {
+        Driver driver = new Driver("이태석");
+        Bus bus = new Bus();
+        Taxi taxi = new Taxi();
+        SportsCar sportsCar = new SportsCar();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("운전할 차를 선택하세요");
+        System.out.println("[1]:버스, [2]택시, [3]스포츠카 : ");
+        int sel = sc.nextInt();
+        switch (sel){
+            case 1 : driver.drive(bus); break;
+            case 2 : driver.drive(taxi); break;
+            case 3 : driver.drive(sportsCar); break;
 
+        }
 
     }
 }
